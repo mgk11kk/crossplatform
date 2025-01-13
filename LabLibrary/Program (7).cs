b70072b8b0eea8493f8aad854a13008e5e14e79a
mgk11kk <158579011+mgk11kk@users.noreply.github.com>
2025-01-14 00:58:15 +0200

    Add files via upload

---
+++ LabLibrary/Program (7).cs
@@ -0,0 +1,27 @@
+﻿using Lab1;
+using Lab2;
+using Lab3;
+
+namespace LabLibrary
+{
+    public static class LabRunner
+    {
+        public static void RunChosenLab(string labName, string inputFilePath, string outputFilePath)
+        {
+            switch (labName)
+            {
+                case "Lab1":
+                    Lab1.Program.RunLab(inputFilePath, outputFilePath);
+                    break;
+                case "Lab2":
+                    Lab2.Program.RunLab(inputFilePath, outputFilePath);
+                    break;
+                case "Lab3":
+                    Lab3.Program.RunLab(inputFilePath, outputFilePath);
+                    break;
+                default:
+                    throw new ArgumentException("Invalid lab name.");
+            }
+        }
+    }
+}
